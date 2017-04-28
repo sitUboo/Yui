c245b517f81bbcfba60becc78781422c4bab1b0e
steven <stevendeal@gmail.com>
2017-04-28 11:15:04 -0400

    Vanity

--- src/main/java/com/cardlytics/scm/Yui/YuiTask.java
+++ src/main/java/com/cardlytics/scm/Yui/YuiTask.java
@@ -19,7 +19,7 @@ import org.mozilla.javascript.ErrorReporter;
 import org.mozilla.javascript.EvaluatorException;
 
 /**
- * Yui processor tool for Ant
+ * Yui processor tool for Ant by me
 */
 
 public class YuiTask extends Task {
